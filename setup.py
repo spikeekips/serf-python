da25da46ab48e87fd14b3d64ca76ab90f86e7591
Spike^ekipS <spikeekips@gmail.com>
2014-03-12 19:13:50 +0900

    updated for submitting `pypi`

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 
 
 setup(
-    name='serf',
+    name='serf-python',
     version='0.1',
     description='serf client for python',
-    long_description=file('README.md', ).read(),
+    long_description="""
+For more details, please see https://github.com/spikeekips/serf-python .
+    """,
     author='Spike^ekipS',
     author_email='spikeekips@gmail.com',
-    url='https://github.com/spikeekips',
+    url='https://github.com/spikeekips/serf-python',
     license='License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
     install_requires=(
             'msgpack-python',
