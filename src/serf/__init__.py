6d71a9cc5e2b3ea0f20ca3c69076d8b6dbb0e695
Spike^ekipS <spikeekips@gmail.com>
2014-03-13 04:10:07 +0900

    added for easy access

--- src/serf/__init__.py
+++ src/serf/__init__.py
@@ -1,5 +1,13 @@
-from .client import Client
+from .client import Client, get_request_class
 Client
+get_request_class
+
 from ._exceptions import *
+
+from . import constant
+constant
+
+from . import request
+request
 
 
