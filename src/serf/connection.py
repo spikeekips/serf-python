f8f977e9b9ee2bde96cdb7d6baa61b96e1ae2559
Spike^ekipS <spikeekips@gmail.com>
2014-03-19 12:40:28 +0900

    Fix for failing build under travis-ci.
    This was caused, the `urlparse` module in travis-ci does not understand the non-standard scheme, like `serf` in URI.
    
    Ran the code like this under travis-ci,
    
    ```python
    >>> uri = 'serf://127.0.0.1:7373/?AuthKey=key'
    >>> import urlparse
    >>> _parsed = urlparse.urlparse(uri, )
    >>> _parsed.path = '/?AuthKey=key'
    >>> _parsed.query = ''
    ```
    
    But it should be in normal environment,
    
    ```python
    >>> _parsed.path = '/'
    >>> _parsed.query = 'AuthKey=key'
    ```

--- src/serf/connection.py
+++ src/serf/connection.py
@@ -33,7 +33,18 @@ def _parse_host (h, ) :
     if not h.startswith('serf://') :
         h = 'serf://%s' % h
     
-    _parsed = urlparse.urlparse(h, )
+    _parsed = list(urlparse.urlparse(h, ), )
+    if _parsed[0] not in ('serf', ) :
+        raise _exceptions.InvalidHostURL('invalid host url, `%s`.' % h, )
+
+    # the `urlparse` module in `travis-cis` does not understand the
+    # non-standard scheme like `serf'.
+    _parsed[0] = 'http'
+    if not _parsed[2].startswith('/') :
+        _parsed[2] = '/' + _parsed[2]
+
+    _parsed = urlparse.urlparse(urlparse.urlunparse(_parsed, ), )
+
     _host = map(
             lambda x : None if x in ('', -1, ) else x,
             urllib.splitnport(_parsed.netloc, defport=7373, ),
